6ddd4adb7aa4b5b47f81ce43466db73450adfb32
Alfio Emanuele Fresta <alfio.emanuele.f@gmail.com>
2016-08-26 11:56:15 +0100

    Initial commit

---
+++ exercise1.py
@@ -0,0 +1,17 @@
+from ecb_lib import encrypt_image_file, decrypt_image_file
+
+
+# A key to use for the Caesar Cipher
+KEY = 42
+
+
+def caesar(image):
+    output = []
+
+    for value in image:
+        output += [value]  # TODO change here (this does nothing)
+
+    return output
+
+
+encrypt_image_file("tux.png", caesar, "tux-caesar.png")

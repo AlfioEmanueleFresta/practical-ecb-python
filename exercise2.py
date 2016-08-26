7843de0c59baf033d6e8d11bb9049868ddec630e
Alfio Emanuele Fresta <alfio.emanuele.f@gmail.com>
2016-08-26 14:49:04 +0100

    Template files for exercise 2 and 3

---
+++ exercise2.py
@@ -0,0 +1,4 @@
+from cp_ecb import get_stream_cipher, encrypt_image_file, decrypt_image_file
+
+
+# TODO Your code goes here.
---
+++ exercise3.py
@@ -0,0 +1,5 @@
+from cp_ecb import get_ecb_encrypter, get_ecb_decrypter,\
+                   encrypt_image_file, decrypt_image_file
+
+
+# TODO Your code goes here.
